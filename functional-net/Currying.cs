9444714dd59f25fc61c0e7d0ff66afb66ab44a71
timothy.goric@gmail.com <timothy.goric@gmail.com@791b9663-1154-8dee-8f3f-8e18014e04d4>
2012-05-03 01:42:20 +0000

    initial commit; curry functions

---
+++ functional-net/Currying.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace FunctionalDotNet
+{
+    public static class Currying
+    {
+        public static Func<T1, Func<T2, TResult>> Curry<T1, T2, TResult>(this Func<T1, T2, TResult> func)
+        {
+            return x => y => func(x, y);
+        }
+        public static Func<T1, Func<T2, Func<T3, TResult>>> Curry<T1, T2, T3, TResult>(this Func<T1, T2, T3, TResult> func)
+        {
+            return x => y => z => func(x, y, z);
+        }
+        public static Func<T1, Func<T2, Func<T3, Func<T4,TResult>>>> Curry<T1, T2, T3, T4, TResult>(this Func<T1, T2, T3, T4, TResult> func)
+        {
+            return w => x => y => z => func(w, x, y, z);
+        }
+        public static Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, TResult>>>>> Curry<T1, T2, T3, T4, T5, TResult>(this Func<T1, T2, T3, T4, T5, TResult> func)
+        {
+            return v => w => x => y => z => func(v, w, x, y, z);
+        }
+        public static Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, TResult>>>>>> Curry<T1, T2, T3, T4, T5, T6, TResult>(this Func<T1, T2, T3, T4, T5, T6, TResult> func)
+        {
+            return u => v => w => x => y => z => func(u, v, w, x, y, z);
+        }
+        public static Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, TResult>>>>>>> Curry<T1, T2, T3, T4, T5, T6, T7, TResult>(this Func<T1, T2, T3, T4, T5, T6, T7, TResult> func)
+        {
+            return t => u => v => w => x => y => z => func(t, u, v, w, x, y, z);
+        }
+        public static Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, Func<T8, TResult>>>>>>>> Curry<T1, T2, T3, T4, T5, T6, T7, T8, TResult>(this Func<T1, T2, T3, T4, T5, T6, T7, T8, TResult> func)
+        {
+            return s => t => u => v => w => x => y => z => func(s, t, u, v, w, x, y, z);
+        }
+    }
+}
