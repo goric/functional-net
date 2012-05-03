736c33b2b2bfa560901ba0caa1ac63def81fe5ba
timothy.goric@gmail.com <timothy.goric@gmail.com@791b9663-1154-8dee-8f3f-8e18014e04d4>
2012-05-03 01:42:20 +0000

    initial commit; curry functions

---
+++ trunk/functional-net/PartialApplication.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+namespace FunctionalDotNet
+{
+    public static class PartialApplication
+    {
+        public static void PartiallyApply<T1, TResult>(this Func<T1,TResult> func)
+        {
+            
+        }
+    }
+}
