61aa4077b11072c6a6b5f368af70436b0b3e7593
timothy.goric@gmail.com <timothy.goric@gmail.com@791b9663-1154-8dee-8f3f-8e18014e04d4>
2012-05-04 01:17:34 +0000

    implementations for most of the functions available on F#'s Seq.

--- functional-net/Aliases.cs
+++ functional-net/Aliases.cs
@@ -4,9 +4,18 @@ using System.Linq;
 
 namespace FunctionalDotNet
 {
+    /// <summary>
+    /// Wrappers on existing IEnumerable functions, re-named with their standard functional names
+    /// </summary>
     public static class Aliases
     {
+        #region Map
+
         public static IEnumerable<T2> Map<T, T2>(this IEnumerable<T> enumerable, Func<T, T2> selector)
+        {
+            return enumerable.Select(selector);
+        }
+        public static IEnumerable<T2> Mapi<T, T2>(this IEnumerable<T> enumerable, Func<T, int, T2> selector)
         {
             return enumerable.Select(selector);
         }
@@ -15,21 +24,34 @@ namespace FunctionalDotNet
             return enumerable.Zip(other, selector);
         }
 
-        public static T Fold<T>(this IEnumerable<T> enumerable, Func<T, T, T> func)
+        #endregion
+
+        #region Fold / Reduce
+
+        public static T Reduce<T>(this IEnumerable<T> enumerable, Func<T, T, T> func)
         {
             return enumerable.Aggregate(func);
         }
-        public static T2 Fold<T, T2>(this IEnumerable<T> enumerable, T2 initval, Func<T2, T, T2> func)
+        public static T2 Reduce<T, T2>(this IEnumerable<T> enumerable, T2 initval, Func<T2, T, T2> func)
         {
             return enumerable.Aggregate(initval, func);
         }
+
+        public static T Fold<T>(this IEnumerable<T> enumerable, Func<T, T, T> func)
+        {
+            return Reduce(enumerable, func);
+        }
+        public static T2 Fold<T, T2>(this IEnumerable<T> enumerable, T2 initval, Func<T2, T, T2> func)
+        {
+            return Reduce(enumerable, initval, func);
+        }
         public static T FoldL<T>(this IEnumerable<T> enumerable, Func<T, T, T> func)
         {
-            return Fold(enumerable, func);
+            return Reduce(enumerable, func);
         }
         public static T2 FoldL<T, T2>(this IEnumerable<T> enumerable, T2 initval, Func<T2, T, T2> func)
         {
-            return Fold(enumerable, initval, func);
+            return Reduce(enumerable, initval, func);
         }
 
         public static T FoldR<T>(this IEnumerable<T> enumerable, Func<T, T, T> func)
@@ -39,6 +61,21 @@ namespace FunctionalDotNet
         public static T2 FoldR<T, T2>(this IEnumerable<T> enumerable, T2 initval, Func<T2, T, T2> func)
         {
             return enumerable.Reverse().Aggregate(initval, func);
+        }
+
+        #endregion
+
+        public static IEnumerable<T> Filter<T>(this IEnumerable<T> enumerable, Func<T, bool> pred)
+        {
+            return enumerable.Where(pred);
+        }
+        public static T Find<T>(this IEnumerable<T> enumerable, Func<T, bool> pred)
+        {
+            return enumerable.First(pred);
+        }
+        public static T Head<T>(this IEnumerable<T> enumerable)
+        {
+            return enumerable.First();
         }
     }
 }
