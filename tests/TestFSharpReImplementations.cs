206eb4d5cc213db08d88baf3a8fa0ca62c1aa6d6
timothy.goric@gmail.com <timothy.goric@gmail.com@791b9663-1154-8dee-8f3f-8e18014e04d4>
2012-05-09 01:36:09 +0000

    Adding test project, starting tests

---
+++ tests/TestFSharpReImplementations.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace tests
+{
+    [TestClass]
+    public class TestFSharpReImplementations
+    {
+    }
+}
---
+++ tests/TestOption.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using FunctionalDotNet;
+
+namespace tests
+{
+    [TestClass]
+    public class TestOption
+    {
+    }
+}
